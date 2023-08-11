a770d7c532a246813b4440552dcb3f195ac0708d
VegaZie <davidgerardovegas29@gmail.com>
2023-08-11 14:28:34 -0600

    Se insertaron los espacio para el número de telefono y un apartado para controlar si tiene activada la verificación en dos pasos

--- models/userModel.js
+++ models/userModel.js
@@ -1,11 +1,13 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const userSchema = new mongoose.Schema({
-    name: { type: String },
-    email: { type: String },
-    password: {type: String},
-    role: {type: String},
-    verified : {type: Boolean}
-  });
-  
-  module.exports = mongoose.model('User', userSchema);
+  name: { type: String },
+  email: { type: String },
+  password: { type: String },
+  role: { type: String },
+  verified: { type: Boolean },
+  phoneNumber: { type: String },
+  twoStepVerification: { type: Boolean },
+});
+
+module.exports = mongoose.model("User", userSchema);
