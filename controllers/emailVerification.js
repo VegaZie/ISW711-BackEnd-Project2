b400f6c3c2d7acde2b61fbb05ece1f9b3fd6efea
VegaZie <davidgerardovegas29@gmail.com>
2023-08-14 14:50:40 -0600

    Se actulizo la función de verificación de email debido a un error a la hora de validar el jwt

--- controllers/emailVerification.js
+++ controllers/emailVerification.js
@@ -4,19 +4,23 @@ const { userPatchVerified } = require("./userController.js");
 
 function verifyEmailToken(req, res) {
   try {
-    const decoded = jwt.verify(req.query.token, secretKey);
-    const user = { verified: true };
-    // Llama a la función userPatchVerified para actualizar el usuario en la base de datos
-    userPatchVerified(decoded.id_user, user, (err, updatedUser) => {
-      if (err) {
-        console.error("Error al actualizar el usuario:", err);
-        res.status(500).json({ error: "Error al actualizar el usuario" });
+    jwt.verify(req.query.token, secretKey, (err, decodedToken) => {
+      if (err || !decodedToken) {
+        res.status(500).json({ error: "El token enviado es invalido" });
       } else {
-        res.status(200).json({ message: "Usuario verificado correctamente" });
+        const user = { verified: true };
+        userPatchVerified(decodedToken.id_user, user, (err, updatedUser) => {
+          if (err) {
+            console.error("Error al actualizar el usuario:", err);
+            res.status(500).json({ error: "Error al verificar el email" });
+          } else {
+            res
+              .status(200)
+              .json({ message: "Usuario verificado correctamente" });
+          }
+        });
       }
     });
-    
-    return decoded.id_user;
   } catch (error) {
     return null; // Retorna null si el token no es válido
   }
