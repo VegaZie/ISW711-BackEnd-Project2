1aec410bdf33186361029192108bb01f595442b6
VegaZie <davidgerardovegas29@gmail.com>
2023-08-14 14:54:10 -0600

    cambio de codigo de status

--- controllers/emailVerification.js
+++ controllers/emailVerification.js
@@ -6,13 +6,13 @@ function verifyEmailToken(req, res) {
   try {
     jwt.verify(req.query.token, secretKey, (err, decodedToken) => {
       if (err || !decodedToken) {
-        res.status(500).json({ error: "El token enviado es invalido" });
+        res.status(404).json({ error: "El token enviado es invalido" });
       } else {
         const user = { verified: true };
         userPatchVerified(decodedToken.id_user, user, (err, updatedUser) => {
           if (err) {
             console.error("Error al actualizar el usuario:", err);
-            res.status(500).json({ error: "Error al verificar el email" });
+            res.status(404).json({ error: "Error al verificar el email" });
           } else {
             res
               .status(200)
